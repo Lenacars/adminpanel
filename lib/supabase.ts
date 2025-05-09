d9b67e8632629f22fb1f1939336e5f3e53685b19
Lenacars <info@lenacars.com>
2025-05-09 16:45:07 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Normal kullanıcılar için
-export const supabase = createClient(supabaseUrl, anonKey);
-
-// Admin (backend işlemleri) için
-export const supabaseAdmin = createClient(supabaseUrl, serviceKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
